89518c9d30c86886d0a46fcb3e7b5738160435a6
Prince Pandey <princekupandey786@gmail.com>
2021-08-01 19:34:34 +0530

    functionality to drawable item

--- app/src/main/java/com/example/android/newsupdate/activity/MainActivity.kt
+++ app/src/main/java/com/example/android/newsupdate/activity/MainActivity.kt
@@ -1,18 +1,71 @@
 package com.example.android.newsupdate.activity
 
+import FirestoreClass
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.example.android.newsupdate.R
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar.*
 import kotlinx.android.synthetic.main.botton_menu.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() , NavigationView.OnNavigationItemSelectedListener{
+
+    companion object{
+        const val MY_PROFILE_REQUEST_CODE: Int = 11
+        const val CREATE_BOARD_REQUEST_CODE: Int = 12
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_sport.setOnClickListener {
-            startActivity(Intent(this,sports_activity::class.java))
+        setupActionBar()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+
+
+    }
+
+
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar_main_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbar_main_activity.setNavigationOnClickListener {
+            //Toggle drawer
+            toggleDrawer()
         }
     }
+
+    private fun toggleDrawer() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_my_profile ->{
+                 Toast.makeText(this,"Open MyProfileActivity",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return true
+    }
+
+
 }
---
+++ app/src/main/java/com/example/android/newsupdate/activity/covid_cases.kt
@@ -0,0 +1,12 @@
+package com.example.android.newsupdate.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.newsupdate.R
+
+class covid_cases : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_covid_cases)
+    }
+}
